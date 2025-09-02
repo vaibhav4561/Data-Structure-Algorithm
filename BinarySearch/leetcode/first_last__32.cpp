b7d1059729f324623b4ad97634dfdb633cdaf4bf
Vaibhav Jain <jainv4561@gmail.com>
2025-09-02 19:27:32 +0530

    first last occurance LC 32

---
+++ BinarySearch/leetcode/first_last__32.cpp
@@ -0,0 +1,64 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            first = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return first;
+}
+
+int lastOccurrence(vector<int> &arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int last = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] == k) {
+            last = mid;
+            //look for larger index on the right
+            low = mid + 1;
+        }
+        else if (arr[mid] < k) {
+            low = mid + 1; // look on the right
+        }
+        else {
+            high = mid - 1; // look on the left
+        }
+    }
+    return last;
+}
+
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstOccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastOccurrence(arr, n, k);
+    return {first, last};
+}
+
+
+int main(){
+     vector<int> arr = {5,7,7,8,8,10};
+    int n = 6, target = 8;
+
+    pair<int,int> ans = firstAndLastPosition(arr,n,target);
+    cout<<ans.first<<" "<<ans.second;
+}
